dcba194adb1e0ffb0de5223ecff2fc9322829431
Stephen O'Loughlin <soloughlin@virbela.com>
2019-08-30 17:44:31 -0700

    wallrunning direction being set properly

--- Source/TestingGrounds/Private/WallRunning.cpp
+++ Source/TestingGrounds/Private/WallRunning.cpp
@@ -68,88 +68,70 @@ void UWallRunning::TickComponent(float DeltaTime, ELevelTick TickType, FActorCom
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// run line traces to find walls
-	TArray<AActor*> list;
-	list.Add(player);
-	FVector startLoc = player->GetActorLocation();
-	FVector rightVect = player->GetActorRightVector().GetSafeNormal();
-	FHitResult rightHit;
-	if (
-		GetWorld()->LineTraceSingleByChannel(rightHit, startLoc, startLoc + 75.0f * rightVect, ECollisionChannel::ECC_Visibility) && rightHit.Actor->ActorHasTag("Wall")
-		) {
-		playerDir = FVector::CrossProduct(rightHit.ImpactNormal, FVector(0.0f, 0.0f, -1.0f));
-		//DrawDebugLine(GetWorld(), player->GetActorLocation(), player->GetActorLocation() + 100.0f * playerDir, FColor::Red, true, 10.0f);
-		if (!onWall && player->GetCharacterMovement()->IsFalling()) {
-			SetOnWall(true);
-			wallRunTimeline->PlayFromStart();
-			UE_LOG(LogTemp, Warning, TEXT("%s"), onWall ? TEXT("ON WALL") : TEXT("NOT ON WALL"));
-		}
-	}
-	else {
-		SetOnWall(false);
-	}
-
+	// Fire raycasts to left and right of player
 	FHitResult leftHit;
-	if (
-		GetWorld()->LineTraceSingleByChannel(leftHit, startLoc, startLoc + -75.0f * rightVect, ECollisionChannel::ECC_Visibility) && leftHit.Actor->ActorHasTag("Wall")
-		) {
-		playerDir = FVector::CrossProduct(leftHit.ImpactNormal, FVector(0.0f, 0.0f, 1.0f));
-		if (!onWall && player->GetCharacterMovement()->IsFalling()) {
-			SetOnWall(true);
-			wallRunTimeline->PlayFromStart();
-			UE_LOG(LogTemp, Warning, TEXT("%s"), onWall ? TEXT("ON WALL") : TEXT("NOT ON WALL"));
+	FHitResult rightHit;
+	FVector startLoc = player->GetActorLocation();
+	FVector rightVect = player->GetActorRightVector();
+	bool contactLeft = false;
+	bool contactRight = false;
+	FHitResult finalHitResult;
+	bool finalHitIsLeft = false;
+	contactLeft = GetWorld()->LineTraceSingleByChannel(leftHit, startLoc, startLoc - 75.0f * rightVect, ECollisionChannel::ECC_Visibility);
+	contactRight = GetWorld()->LineTraceSingleByChannel(rightHit, startLoc, startLoc + 75.0f * rightVect, ECollisionChannel::ECC_Visibility);
+
+	// If actor is tagged as a wall and the impact normal's z coordinate is less than 0.1f
+	if (contactLeft)
+		contactLeft = contactLeft && leftHit.ImpactNormal.Z < 0.1f && leftHit.ImpactNormal.Z >= -0.05f && leftHit.Actor->ActorHasTag("Wall");
+
+	if (contactRight)
+		contactRight = contactRight && rightHit.ImpactNormal.Z < 0.1f && rightHit.ImpactNormal.Z >= -0.05f && rightHit.Actor->ActorHasTag("Wall");
+	// if both raycasts hit an acceptable surface, discard the further one
+	if (contactLeft && contactRight) {
+		if ((leftHit.ImpactPoint - startLoc).Size() < (rightHit.ImpactPoint - startLoc).Size()) {
+			contactRight = false;
+			finalHitIsLeft = true;
+			finalHitResult = leftHit;
+		}
+		else {
+			contactLeft = false;
+			finalHitIsLeft = false;
+			finalHitResult = rightHit;
 		}
 	}
 	else {
-		SetOnWall(false);
+		if (contactLeft) {
+			finalHitIsLeft = true;
+			finalHitResult = leftHit;
+		}
+		else if (contactRight) {
+			finalHitIsLeft = false;
+			finalHitResult = rightHit;
+		}
 	}
 
-	DrawDebugLine(GetWorld(), startLoc, startLoc + 75.0f * rightVect, FColor::Red, false);
-	DrawDebugLine(GetWorld(), startLoc, startLoc + -75.0f * rightVect, FColor::Red, false);
+	// set wallrunning direction based on impact normal
+	if (contactLeft || contactRight) {
+		if(finalHitIsLeft)
+			playerDir = FVector::CrossProduct(finalHitResult.ImpactNormal, FVector::UpVector);
+		else
+			playerDir = FVector::CrossProduct(finalHitResult.ImpactNormal, FVector::DownVector);
+	}
+	
+	// if not in wallrunning state, enter wallrunning state (start wallrunning timeline)
+	//if (!wallRunTimeline->IsPlaying())
+		//wallRunTimeline->PlayFromStart();
 }
 
 void UWallRunning::WallRunUpdate(float val) {
-	if ((UGameplayStatics::GetPlayerController(GetWorld(), 0)->GetInputKeyTimeDown(EKeys::W) > 0.0f
-		|| UGameplayStatics::GetPlayerController(GetWorld(), 0)->GetInputKeyTimeDown(EKeys::S) > 0.0f) && onWall) {
-		if (GEngine)
-			GEngine->AddOnScreenDebugMessage(0, 5.0f, FColor::Red, FString("AAAAAAAAAAA"), true);
-		player->GetCharacterMovement()->GravityScale = 0.0f;
-		player->GetCharacterMovement()->SetPlaneConstraintNormal(FVector(0.0f, 0.0f, 1.0f));
-		player->GetCharacterMovement()->AddForce(playerDir * 20000.0f);
-	}
-	else {
-		wallRunTimeline->Stop();
-		TurnOffWallRunning();
-	}
+	//lock player to plane, disable gravity, and move them along the wallrunning direction
 }
 
 void UWallRunning::SmoothRunRightUpdate(float val) {
-	UGameplayStatics::GetPlayerController(GetWorld(), 0)->SetControlRotation(
-		FMath::Lerp(player->GetActorRotation(), 
-		FRotator(player->GetActorRotation().Pitch, player->GetActorRotation().Yaw, -20.0f), 
-		val)
-	);
+
 }
 
 void UWallRunning::SmoothRunLeftUpdate(float val) {
-	UGameplayStatics::GetPlayerController(GetWorld(), 0)->SetControlRotation(
-		FMath::Lerp(player->GetActorRotation(),
-			FRotator(player->GetActorRotation().Pitch, player->GetActorRotation().Yaw, 20.0f),
-			val)
-	);
-}
 
-void UWallRunning::TurnOffWallRunning() {
-	player->GetCharacterMovement()->GravityScale = 1.0f;
-	player->GetCharacterMovement()->SetPlaneConstraintNormal(FVector(0.0f, 0.0f, 0.0f));
-	SetOnWall(false);
-}
-
-void UWallRunning::SetOnWall(bool val) {
-	onWall = val;
-	if (val)
-		player->DisableHorizontalMovement();
-	else
-		player->EnableHorizontalMovement();
 }
 
